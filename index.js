e5971ebba20f74aec3ecb3463ddb69e05b793722
hoangak47 <hoang41911@gmail.com>
2025-03-06 23:39:37 +0700

    a

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ function replaceTextInDOM(node) {
   }
 }
 
+document.querySelectorAll("a.logo-header img").forEach((img) => {
+  img.src =
+    "https://vsktravel.com/thumbs/360x75x1/upload/photo/final-logo-2001-2260.png";
+});
+
 replaceTextInDOM(document.body);
