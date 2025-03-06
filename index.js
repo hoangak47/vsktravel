c34a9db1b604dbc9f81ac4a9a9672521393814b9
hoangak47 <hoang41911@gmail.com>
2025-03-06 23:27:26 +0700

    up

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-document.body.innerHTML = document.body.innerHTML.replace(/VIETSKY TOURIST/g, "VSK TRAVEL & GUIDE");
+function replaceTextInDOM(node) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    node.nodeValue = node.nodeValue.replace(
+      /VIETSKY TOURIST/g,
+      "VSK TRAVEL & GUIDE"
+    );
+  } else {
+    node.childNodes.forEach(replaceTextInDOM);
+  }
+}
+
+replaceTextInDOM(document.body);
