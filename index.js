aacbfc441684048b56402cfbc0ce59831675710f
hoangak47 <hoang41911@gmail.com>
2025-03-07 17:14:14 +0700

    up

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-function replaceTextInDOM(node) {
-  if (node.nodeType === Node.TEXT_NODE) {
-    node.nodeValue = node.nodeValue.replace(
-      /VIETSKY TOURIST/gi,
-      "VSK TRAVEL & GUIDE"
-    );
-  } else {
-    node.childNodes.forEach(replaceTextInDOM);
-  }
-}
-
-document.querySelectorAll("a.logo-header img").forEach((img) => {
-  img.src =
-    "https://vsktravel.com/thumbs/360x75x1/upload/photo/final-logo-2001-2260.png";
-});
-
-replaceTextInDOM(document.body);
+const style = document.createElement("style");
+style.innerHTML = `
+    @media (max-width: 1024px) {
+        a.logo-header {
+            width: 90px !important;
+        }
+    }
+`;
+document.head.appendChild(style);
